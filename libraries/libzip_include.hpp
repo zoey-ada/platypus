6b8180a4873b825c62b6663c6e970d85d15ce1b0
Zoey Goetzinger <zoeyag154252@gmail.com>
2022-01-09 21:03:12 -0600

    3rd party: add zlib and libzip libraries

---
+++ libraries/libzip_include.hpp
@@ -0,0 +1,17 @@
+#pragma warning(push)
+// #pragma warning(disable : 4068)
+// #pragma warning(disable : 4100)
+// #pragma warning(disable : 4127)
+// #pragma warning(disable : 4146)
+// #pragma warning(disable : 4244)
+// #pragma warning(disable : 4267)
+// #pragma warning(disable : 4512)
+// #pragma warning(disable : 4800)
+// #pragma warning(disable : 5054)
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wnullability-completeness"
+
+#include <zip.h>
+
+#pragma GCC diagnostic pop
+#pragma warning(pop)
