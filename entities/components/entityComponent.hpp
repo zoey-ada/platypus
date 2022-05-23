9559dd4e80d0d6f0df02cea41c51b66fa060af18
Zoey Goetzinger <zoeyag154252@gmail.com>
2022-05-22 23:38:33 -0500

    entity: initial commit for entity and factory

---
+++ entities/components/entityComponent.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+
+#include <utilities/time.hpp>
+
+class Entity;
+
+namespace google
+{
+namespace protobuf
+{
+class Message;
+}
+}
+
+using Message = google::protobuf::Message;
+
+class EntityComponent
+{
+	friend class EntityFactory;
+
+public:
+	EntityComponent() = default;
+	virtual ~EntityComponent() = default;
+
+	virtual bool initialize(const std::shared_ptr<Message>& data) = 0;
+
+	virtual void postInitialize() {}
+	virtual void update(Milliseconds /*delta*/) {}
+
+	virtual const char* getName() const = 0;
+
+protected:
+	std::shared_ptr<Entity> _owner;
+
+private:
+	void setOwner(std::shared_ptr<Entity> owner) { this->_owner = owner; }
+};
---
+++ entities/entityFactory.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <string>
+
+#include "entity.hpp"
+
+namespace google
+{
+namespace protobuf
+{
+class Message;
+}
+}
+
+using Message = google::protobuf::Message;
+
+class EntityComponent;
+
+using EntityComponentCreator = std::function<std::shared_ptr<EntityComponent>()>;
+using EntityComponentCreatorMap = std::map<std::string, EntityComponentCreator>;
+
+class EntityFactory
+{
+public:
+	EntityFactory();
+	virtual ~EntityFactory() = default;
+
+	std::shared_ptr<Entity> createEntity(const char* entity_resource);
+
+private:
+	EntityId _last_entity_id {InvalidEntityId};
+	EntityComponentCreatorMap _entity_component_creators {};
+
+	std::shared_ptr<EntityComponent> createComponent(std::shared_ptr<Message>& data);
+
+	inline EntityId getNextEntityId()
+	{
+		++this->_last_entity_id;
+		return this->_last_entity_id;
+	}
+};
