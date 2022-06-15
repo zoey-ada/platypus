e9bd7406d3f08f500fc72bb15011a3cf1191f9fa
Zoey Goetzinger <zoeyag154252@gmail.com>
2022-06-14 21:04:56 -0500

    logic: add game states and entity loading

--- baseGameLogic.cpp
+++ baseGameLogic.cpp
@@ -1,10 +1,14 @@
 #include "baseGameLogic.hpp"
 
-#include "views/iView.hpp"
+#include <entities/components/transformComponent3d.hpp>
+#include <entities/entityFactory.hpp>
 #include <utilities/logging/logger.hpp>
+
+#include "views/iView.hpp"
 
 bool BaseGameLogic::initialize()
 {
+	this->_entity_factory = std::make_shared<EntityFactory>();
 	return true;
 }
 
@@ -24,7 +28,60 @@ void BaseGameLogic::removeView(const std::shared_ptr<IView>& view)
 
 void BaseGameLogic::onUpdate(Milliseconds delta)
 {
+	switch (this->_state)
+	{
+	case GameState::Initializing:
+	{
+		this->changeState(GameState::MainMenu);
+		break;
+	}
+	case GameState::MainMenu:
+	{
+		// auto cyndaquil = this->createEntity("cyndaquil.json");
+		// auto cube = this->createEntity("cube.json");
+		// auto component = cube->getComponent<TransformComponent3d>("transform_component_3d");
+		// component.lock()->registerEventSinks();
+
+		for (const auto& view : this->_views)
+			view->onRestore();
+
+		this->changeState(GameState::Running);
+		break;
+	}
+	case GameState::Loading:
+	{
+		break;
+	}
+	case GameState::Running:
+	{
+		for (const auto& view : this->_views)
+			view->onUpdate(delta);
+
+		for (const auto& entity : this->_entities)
+			entity.second->update(delta);
+
+		break;
+	}
+	default:
+		throw std::exception("The game entered a state that could not be handled.");
+	}
 	// TODO: game states
-	for (const auto& view : this->_views)
-		view->onUpdate(delta);
+}
+
+std::shared_ptr<Entity> BaseGameLogic::createEntity(const char* entity_resource)
+{
+	auto entity = this->_entity_factory->createEntity(entity_resource);
+
+	if (entity != nullptr)
+	{
+		this->_entities.insert(std::make_pair(entity->getId(), entity));
+	}
+
+	return entity;
+}
+
+void BaseGameLogic::changeState(const GameState new_state)
+{
+	// todo: stuffs
+	this->_state = new_state;
 }
--- baseGameLogic.hpp
+++ baseGameLogic.hpp
@@ -1,12 +1,21 @@
 #pragma once
 
-#include "entities/entity.hpp"
-#include "utilities/time.hpp"
-
 #include <list>
 #include <memory>
 
+#include <entities/entity.hpp>
+#include <utilities/time.hpp>
+
 class IView;
+class EntityFactory;
+
+enum class GameState
+{
+	Initializing,
+	MainMenu,
+	Loading,
+	Running,
+};
 
 class BaseGameLogic
 {
@@ -15,14 +24,24 @@ public:
 	virtual ~BaseGameLogic() = default;
 
 	bool initialize();
+	void deinitialize();
 
-	virtual void addView(const std::shared_ptr<IView>& view, const EntityId entityId = InvalidEntityId);
+	virtual void addView(const std::shared_ptr<IView>& view,
+		const EntityId entityId = InvalidEntityId);
 	virtual void removeView(const std::shared_ptr<IView>& view);
 
 	virtual void onUpdate(Milliseconds delta);
 
+	std::shared_ptr<Entity> createEntity(const char* entity_resource);
+
+	virtual void changeState(const GameState new_state);
+	const GameState getState() const { return _state; }
+
 	[[nodiscard]] std::list<std::shared_ptr<IView>> views() const { return this->_views; }
 
 private:
+	GameState _state {GameState::Initializing};
 	std::list<std::shared_ptr<IView>> _views;
+	std::shared_ptr<EntityFactory> _entity_factory;
+	std::map<EntityId, std::shared_ptr<Entity>> _entities;
 };
