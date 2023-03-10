fa30aef74cbf96e475e927507d54884b4688ad29
Zoey Goetzinger <zoeyag154252@gmail.com>
2023-03-09 21:27:47 -0600

    tests: add res cache unit tests

---
+++ tests/resource_cache/loaders/mockResourceLoader.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <resource_cache/loaders/iResourceLoader.hpp>
+
+class MockResourceLoader: public IResourceLoader
+{
+public:
+	virtual ~MockResourceLoader() = default;
+
+	std::string getPattern() override { return this->_pattern; }
+
+	ResourceType getType() override { return this->_resource_type; }
+
+	std::shared_ptr<Resource> load(const char* resource_id, const char* store_id,
+		std::byte* resource_data, const uint64_t data_size) override
+	{
+		return this->_resource;
+	}
+
+	uint8_t* allocate(unsigned int size) override { return this->_data; }
+
+	std::string _pattern {"*"};
+	ResourceType _resource_type {ResourceType::General};
+	std::shared_ptr<Resource> _resource {nullptr};
+	uint8_t* _data {nullptr};
+};
